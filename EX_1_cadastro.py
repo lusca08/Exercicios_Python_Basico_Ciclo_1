0c95041437fcafc89c2f1a86c691888ec59ebd33
lusca <costaalveslucas022@gmail.com>
2025-06-10 15:52:40 -0300

    Add files via upload

--- EX_1_cadastro.py
+++ EX_1_cadastro.py
@@ -18,3 +18,21 @@
 
 # ------------------------------------------ ESCREVA SEU CÓDIGO ABAIXO -----------------------------------------------------------
 
+nome = input('Nome: ')
+idade = input('idade: ')
+email = input('email: ')
+senha = input('senha: ')
+
+print(f'| {"-"*30} |')
+print(f'| {"-"*10} CADASTRO {10*'-'} |')
+print(f'| {"-"*30} |')
+print('| nome: ', nome)
+print('| idade: ', idade)
+print('| email: ', email)
+print('| senha: ', senha)
+print('')
+print(f'| {"-"*30} |')
+print(f'| {"-"*5} USUÁRIO CADASTRADO {5*'-'} |')
+print('| Seja bem vindo(a)', nome )
+print('|email: ', email)
+print(f'| {"-"*30} |')
