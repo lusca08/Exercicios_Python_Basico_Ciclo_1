697a8429656061a2cc3fa1600b0253ad1636ab81
lusca <costaalveslucas022@gmail.com>
2025-06-11 15:52:02 -0300

    Add files via upload

--- EX_7_calculo_imc.py
+++ EX_7_calculo_imc.py
@@ -12,3 +12,8 @@
 
 # ------------------------------------------ ESCREVA SEU CÓDIGO ABAIXO -----------------------------------------------------------
 
+
+altura = float(input('Digite sua altura: ')) 
+peso = float(input('Digite seu peso: '))
+imc = peso / (altura ** 2)
+print('O seu IMC é: ', {imc})
